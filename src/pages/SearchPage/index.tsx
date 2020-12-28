4b0ba506d1e169ca2dfbc7cd76fbd45b36eb65e5
Shiro Neko <tagiki5@gmail.com>
2020-12-27 23:47:59 -0300

    Animções de pesquisa

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import './styles.css';
 
 function SearchPage(){
+  const [search, setSearch] = useState('');
+  const [searchA, setSearchA] = useState('');
+  const [btnSearch, setBtnSearch] = useState('d-hidden');
+
+  function searchActive(a: string){
+    setSearch(a);
+    a !== '' ? setSearchA('active') : setSearchA('');
+    a !== '' ? setBtnSearch('btn-search') : setBtnSearch('d-hidden');
+  }
+
+  function searchBook(){
+    
+  }
+
   return (
     <div id="SearchPage">
-      <h1>Página de busca</h1>
-      <div className="cntr">
-        <div className="cntr-innr">
-          <label className="search" htmlFor="inpt_search">
-            <input id="inpt_search" type="text" />
-          </label>
+      <div className="src-cont">
+        <div className="cntr">
+          <div className="cntr-innr">
+            <label className={`search ${searchA}`}>
+              <input 
+                id="inpt_search"
+                type="text"
+                value={search}
+                onChange={(e) => {searchActive(e.target.value)}}
+              />
+            </label>
+          </div>
         </div>
+        <a className={btnSearch} onClick={searchBook}><i className="far fa-arrow-alt-circle-right"></i></a>
       </div>
     </div>
   );
